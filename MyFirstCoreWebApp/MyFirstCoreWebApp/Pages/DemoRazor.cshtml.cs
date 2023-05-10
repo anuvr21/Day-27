26c5a3a8c7b56387b5c98d0f24e122c3f36de8f8
anuvr21 <129931334+anuvr21@users.noreply.github.com>
2023-05-10 17:16:00 +0530

    Add files via upload

---
+++ MyFirstCoreWebApp/MyFirstCoreWebApp/Pages/DemoRazor.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MyFirstCoreWebApp.Pages
+{
+    public class DemoRazorModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
